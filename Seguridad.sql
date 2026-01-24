d06dda05ecb0dcfbe4849494a73195dae745356c
Marckiti <arayaarpimarcos@gmail.com>
2026-01-24 12:15:44 -0500

    Refactor SQL for client search and user creation
    Updated SQL statements for client search and user management, including injection control and password hashing.

--- Seguridad.sql
+++ Seguridad.sql
@@ -1,30 +1,32 @@
-PREPARE stmt FROM 
+-- ======CONTROL DE INYECCION=================================================================================
+PREPARE buscarCliente FROM 
 'SELECT * FROM Cliente WHERE Cedula = ?';
 
+-- Prueba exitosa :)
 SET @cedula = '1756084479';
-EXECUTE stmt USING @cedula;
+EXECUTE buscarCliente USING @cedula;
 
+-- Prueba de ataque :O
+SET @cedula = '1756084479 OR 1=1 --';
+EXECUTE buscarCliente USING @cedula;
 
--- ==============================================
+-- =======HASH DE CONTRASEÑA==================================================================
 CREATE TABLE Usuario (
     ID_Usuario INT AUTO_INCREMENT PRIMARY KEY,
     Usuario VARCHAR(50) UNIQUE NOT NULL,
     Password_Hash VARCHAR(255) NOT NULL
 );
-
+-- Ciframos la contraseña
 INSERT INTO Usuario (Usuario, Password_Hash)
 VALUES ('admin', SHA2('Admin123', 256));
 
--- ====================================================
+-- Vemos la contraseña
+SELECT * FROM Usuario;
+-- Probamos
+SELECT * FROM Usuario
+WHERE Usuario = 'admin'
+AND Password_Hash = SHA2('Admin123', 256);
 
-CREATE USER 'admin_conc'@'localhost' IDENTIFIED BY 'Admin123';
-CREATE USER 'vendedor_conc'@'localhost' IDENTIFIED BY 'Vendedor123';
-
-GRANT ALL PRIVILEGES ON Concesionario.* 
-TO 'admin_conc'@'localhost';
-
-
-GRANT SELECT, INSERT ON Concesionario.Venta 
 TO 'vendedor_conc'@'localhost';
 
 FLUSH PRIVILEGES;
